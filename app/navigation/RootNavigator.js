3b4bf26122c4a6e40a09698a641340d9047cbbc1
Pikser <32457085+Pikser@users.noreply.github.com>
2018-12-25 19:25:12 +0200

    RootNavigator

--- app/navigation/RootNavigator.js
+++ app/navigation/RootNavigator.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import AppNavigator from './AppNavigator';
+import { createStackNavigator } from 'react-navigation';
+import screens from './screens';
 
-const RootNavigator = () => <AppNavigator />;
+import AppNavigator from './AppNavigator';
+import AddProductNavigator from './AddProductNavigator';
+
+const routes = {
+  [screens.App]: AppNavigator, // your tab navigator
+  [screens.AddProduct]: AddProductNavigator,
+};
+
+const RootNavigator = createStackNavigator(routes, {
+  headerMode: 'none',
+  mode: 'modal',
+});
 
 export default RootNavigator;
