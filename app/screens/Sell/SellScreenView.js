81b313e5d00d9456dc4e8abd5644528e5264ffed
Pikser <32457085+Pikser@users.noreply.github.com>
2018-12-29 12:11:12 +0200

    try safe area in sell screen

--- app/screens/Sell/SellScreenView.js
+++ app/screens/Sell/SellScreenView.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
 
 // import s from './styles';
 
 const SellScreenView = () => (
-  <View>
-    <Text>Sell</Text>
-  </View>
+  <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+    <View>
+      <Text>Sell</Text>
+    </View>
+  </SafeAreaView>
 );
 
 SellScreenView.navigationOptions = {
