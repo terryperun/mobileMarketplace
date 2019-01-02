fdbfb73f4292944041921f85ad496f0e81752927
Pikser <32457085+Pikser@users.noreply.github.com>
2019-01-02 21:28:12 +0200

    change tab icon

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -4,7 +4,14 @@ import {
   navigation,
 } from 'react-navigation';
 import { View } from 'react-native';
-import { Ionicons, FontAwesome } from '@expo/vector-icons';
+// import Icon from '@expo/vector-icons/FontAwesome';
+import {
+  EvilIcons,
+  MaterialCommunityIcons,
+  Ionicons,
+  MaterialIcons,
+  FontAwesome,
+} from '@expo/vector-icons';
 
 import screens from './screens';
 // import HomeNavigator from './HomeNavigator';
@@ -16,7 +23,7 @@ import FavoritesNavigator from './FavoritesNavigator';
 import AddProductNavigator from './AddProductNavigator';
 import InboxNavigator from './InboxNavigator';
 
-// import { colors } from '../styles';
+import { colors } from '../styles';
 
 const routes = {
   // [screens.HomeTab]: HomeNavigator,
@@ -25,52 +32,59 @@ const routes = {
     screen: BrowseNavigator,
     navigationOptions: ({ navigation }) => ({
       tabBarLabel: 'Browse',
-      tabBarIcon: ({ tintColor }) => (
-        <FontAwesome name="search" size={24} color={tintColor} />
-      ),
+      tabBarIcon: ({ focused, tintColor }) =>
+        (focused ? (
+          <FontAwesome name="search" size={24} color={tintColor} />
+        ) : (
+          <Ionicons name="ios-search" size={24} color={tintColor} />
+        )),
     }),
   },
 
   [screens.FavoritesTab]: {
     screen: FavoritesNavigator,
     navigationOptions: ({ navigation }) => ({
-      tabBarLabel: 'Favorites',
-      tabBarIcon: ({ tintColor }) => (
-        <FontAwesome name="heart-o" size={24} color={tintColor} />
-      ),
+      tabBarLabel: 'Saved',
+      tabBarIcon: ({ focused, tintColor }) =>
+        (focused ? (
+          <MaterialCommunityIcons
+            name="bookmark"
+            size={24}
+            color={tintColor}
+          />
+        ) : (
+          <MaterialCommunityIcons
+            name="bookmark-outline"
+            size={24}
+            color={tintColor}
+          />
+        )),
     }),
   },
 
   [screens.SellTab]: {
-    // screen: AddProductNavigator,
     screen: () => null,
     navigationOptions: ({ navigation }) => ({
-      tabBarLabel: 'Sell',
+      tabBarLabel: () => null,
       tabBarIcon: ({ tintColor }) => (
         <View
           style={{
             alignItems: 'center',
             justifyContent: 'center',
-            width: 80,
-            height: 80,
-            borderRadius: 80 / 2,
-            backgroundColor: '#FFFFFF',
-            // backgroundColor: colors.white,
+            width: 60,
+            height: 60,
+            borderRadius: 60 / 2,
+            backgroundColor: '#2C3E50',
+            position: 'absolute',
+            // zIndex: 1,
+            bottom: 18,
+            // left: 0,
+            // right: 0,
           }}
         >
-          {/* <View
-            style={{
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: 70,
-              height: 70,
-              borderRadius: 70 / 2,
-              backgroundColor: '#44B2F1',
-            }}
-          > */}
-          <FontAwesome name="camera" size={24} color={tintColor} />
+          {/* <Icon name="camera" size={24} color={tintColor} /> */}
+          <EvilIcons name="camera" size={39} color={colors.white} />
         </View>
-        // </View>
       ),
       tabBarOnPress: () => navigation.navigate('AddProduct'),
     }),
@@ -80,9 +94,20 @@ const routes = {
     screen: InboxNavigator,
     navigationOptions: ({ navigation }) => ({
       tabBarLabel: 'Inbox',
-      tabBarIcon: ({ tintColor }) => (
-        <FontAwesome name="inbox" size={24} color={tintColor} />
-      ),
+      tabBarIcon: ({ focused, tintColor }) =>
+        (focused ? (
+          <MaterialCommunityIcons
+            name="inbox-arrow-down"
+            size={24}
+            color={tintColor}
+          />
+        ) : (
+          <MaterialCommunityIcons
+            name="inbox"
+            size={24}
+            color={tintColor}
+          />
+        )),
     }),
   },
 
@@ -90,13 +115,31 @@ const routes = {
     screen: ProfileNavigator,
     navigationOptions: ({ navigation }) => ({
       tabBarLabel: 'Profile',
-      tabBarIcon: ({ tintColor }) => (
-        <Ionicons name="md-person" size={24} color={tintColor} />
-      ),
+      tabBarIcon: ({ focused, tintColor }) =>
+        (focused ? (
+          <MaterialCommunityIcons
+            name="account"
+            size={24}
+            color={tintColor}
+          />
+        ) : (
+          <MaterialCommunityIcons
+            name="account-outline"
+            size={24}
+            color={tintColor}
+          />
+        )),
     }),
   },
 };
 
-const AppNavigator = createBottomTabNavigator(routes);
+const AppNavigator = createBottomTabNavigator(routes, {
+  tabBarOptions: {
+    activeTintColor: '#2E4052',
+    style: {
+      height: 48,
+    },
+  },
+});
 
 export default AppNavigator;
