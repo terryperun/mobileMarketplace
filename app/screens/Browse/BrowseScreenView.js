95b73ad4c1b3e9f4da42040e665b6e579d01d59d
Pikser <32457085+Pikser@users.noreply.github.com>
2019-01-17 08:03:39 +0200

    automatic updates

--- app/screens/Browse/BrowseScreenView.js
+++ app/screens/Browse/BrowseScreenView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 import SearchInput from '../../components/SearchInput/SearchInput';
 import Icon from '../../components/Icon/Icon';
 
@@ -7,28 +7,31 @@ import s from './styles';
 
 const BrowseScreenView = () => (
   <View>
-    <Text>search</Text>
-    <Icon iconName="search" />
-    <Text>search-outline</Text>
-    <Icon iconName="search-outline" />
+    <StatusBar barStyle="dark-content" />
+    <View>
+      <Text>search</Text>
+      <Icon iconName="search" />
+      <Text>search-outline</Text>
+      <Icon iconName="search-outline" />
 
-    <Text>saved</Text>
-    <Icon iconName="saved" color="red" />
-    <Text>saved-outline</Text>
-    <Icon iconName="saved-outline" />
+      <Text>saved</Text>
+      <Icon iconName="saved" color="red" />
+      <Text>saved-outline</Text>
+      <Icon iconName="saved-outline" />
 
-    <Text>Camera</Text>
-    <Icon iconName="camera" />
+      <Text>Camera</Text>
+      <Icon iconName="camera" />
 
-    <Text>inbox</Text>
-    <Icon iconName="inbox" />
-    <Text>inbox-outline</Text>
-    <Icon iconName="inbox-outline" />
+      <Text>inbox</Text>
+      <Icon iconName="inbox" />
+      <Text>inbox-outline</Text>
+      <Icon iconName="inbox-outline" />
 
-    <Text>profile</Text>
-    <Icon iconName="profile" />
-    <Text>profile-outline</Text>
-    <Icon iconName="profile-outline" />
+      <Text>profile</Text>
+      <Icon iconName="profile" />
+      <Text>profile-outline</Text>
+      <Icon iconName="profile-outline" />
+    </View>
   </View>
 );
 
